22f1325697d5ee0928c5237ee64c500275067c19
Cloud User <ec2-user@ip-10-0-0-210.us-west-1.compute.internal>
2017-08-17 16:55:25 -0400

    Changed GitHut API Key Password field to Token

--- options.php
+++ options.php
@@ -96,7 +96,7 @@ function save_caweb_options($values = array()){
 function api_menu_option_setup(){
 
 ?>
-<style>table td{padding-left: 0 !important; padding-top: 0 !important}</style>
+<style>table tr td:first-of-type {width: 15px;}</style>
 
 <form id="ca-options-form" action="<?= admin_url('admin.php?page=caweb_api'); ?>" method="POST">
   <?php
@@ -106,14 +106,21 @@ function api_menu_option_setup(){
   ?>
 <div class="wrap">
   <h1>GitHub API Key</h1>
-    <table class="form-table">
-      <tr><td><p><div class="tooltip">Username
-		<span class="tooltiptext">Setting this feature enables us to update the theme through GitHub</span></div></p><input type="text" name="caweb_username" size="50" value="<?php echo get_site_option('caweb_username', ''); ?>"></td></tr>
-      <tr><td><p><div class="tooltip">Password<span class="tooltiptext">Setting this feature enables us to update the theme through GitHub</span></div></p><input type="password" name="caweb_password" size="50" value="<?php echo base64_encode(get_site_option('caweb_password', '')); ?>"></td></tr>
-		</table>
+  <table class="form-table">
+    <tr><td>
+        <div class="tooltip">Username<span class="tooltiptext">Setting this feature enables us to update the theme through GitHub</span></div></td>
+      		<td><input type="text" name="caweb_username" size="50" value="<?php echo get_site_option('caweb_username', ''); ?>" /></td></tr>
+      <tr><td>
+        <div class="tooltip">Token<span class="tooltiptext">Setting this feature enables us to update the theme through GitHub</span></div></td>
+  				<td><input type="password" name="caweb_password" size="50" value="<?php echo base64_encode(get_site_option('caweb_password', '')); ?>" /></td></tr>
+  </table>  
   </div>
   <input type="submit" name="caweb_api_options_submit" id="submit" class="button button-primary" value="<?php _e('Save Changes') ?>" />
-</form>
+ </form>
+  
+  
+  
+
 <?php
 }
 
