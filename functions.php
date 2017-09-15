7948fcaa1d70734f8f3445c1fbbdefc8312aad67
EC2 Default User <ec2-user@ip-10-0-0-109.us-west-1.compute.internal>
2017-09-15 22:38:23 +0000

    Module load order has been swapped

--- functions.php
+++ functions.php
@@ -508,9 +508,9 @@ function caweb_initialize_divi_modules() {
 
 		include(CAWebAbsPath . "/builder/functions.php");
 		include(CAWebAbsPath . "/builder/class-caweb-builder-element.php");
+		include(CAWebAbsPath. '/builder/special-modules.php');
 		include(CAWebAbsPath . "/builder/main-modules.php");
 		include(CAWebAbsPath. '/builder/main-fullwidth-modules.php');
-		include(CAWebAbsPath. '/builder/special-modules.php');
 		include(CAWebAbsPath . "/builder/layouts.php");
 
 }
