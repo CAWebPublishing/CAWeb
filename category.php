77212b6b959bd270ca47f66c881202170a65d695
Danny Guzman <guzmanjd86@gmail.com>
2017-11-30 19:52:39 +0000

    Cleaned up category template file

--- category.php
+++ category.php
@@ -1,43 +1,55 @@
 <?php
+
 get_header();
 $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
+
 ?>
 <body <?php body_class('primary'); ?>>
 <?php get_template_part('partials/content', 'header') ?>
+
 <div id="page-container">
 <div id="et-main-area">
-<div id="main">
-<?php if ( ! $is_page_builder_used ) : ?>
-	<div class="container">
-		<div id="content-area" class="clearfix">
-			<div id="left-area">
-<?php endif; ?>
+
+<div id="main-content" class="main-content <?= ( ! $is_page_builder_used ? 'ca_wp_container' : '' ) ?>">
 <main class="main-primary">
+
 			<?php while ( have_posts() ) : the_post(); ?>
+
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="entry-content">
+
 <div id="skip-to-content"><a href="#main-content">Skip to Main Content</a></div>
+
+
 					<?php
+					if ( "on" == get_post_meta($post->ID, 'ca_custom_post_title_display', true) )
+							print the_title(sprintf('<!-- Page Title--><h1 class="page-title %1$s" >', ( $is_page_builder_used ? 'ca_wp_container' : '' ) ), '</h1>');
+
+							print '<div class="entry-content">';
+
 						the_content();
+
 						if ( ! $is_page_builder_used )
 							wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
-					?>
-					</div> <!-- .entry-content -->
-				<?php
+
+
+							print '</div>';
+
+
+
 					if ( ! $is_page_builder_used && comments_open() && 'on' === et_get_option( 'divi_show_pagescomments', 'false' ) ) comments_template( '', true );
+
 				?>
+
 				</article> <!-- .et_pb_post -->
+
 			<?php endwhile; ?>
-					<span class="return-top"></span>
+					<span class="return-top hidden-print"></span>
 </main>
-<?php if ( ! $is_page_builder_used ) : ?>
-			</div> <!-- #left-area -->
-			<?php get_sidebar(); ?>
-		</div><!-- #content-area -->
-	</div> <!-- .container -->
-<?php endif; ?>
+
 </div> <!-- #main-content -->
 </div>
 </div>
 <?php get_footer(); ?>
+
+
 </body>
