9962fa4a73ff756a29c396202ed8fff1a137f868
Danny Guzman <guzmanjd86@gmail.com>
2017-12-01 01:04:32 +0000

    Corrected CSS on Serp Page

--- page-templates/searchpage.php
+++ page-templates/searchpage.php
@@ -3,11 +3,16 @@
 Template Name: Search Results Page
 */
 
-get_header(); ?>
+get_header(); 
+get_template_part('partials/content', 'header') 
 
-<?php get_template_part('partials/content', 'header') ?>
+?>
 
+<div id="page-container">
+<div id="et-main-area">
 <div id="main-content" class="main-content">
+  
+<main class="main-primary">
   <div id="skip-to-content"><a href="#main-content">Skip to Main Content</a></div>
 
       <div class="section section-default collapsed p-t-lg">
@@ -40,7 +45,8 @@ get_header(); ?>
 <?php endif; ?>
 
 </div> <!-- #main-content -->
-
+</div>
+</div>
 <style>
   button.close.close-search {
     visibility: hidden;
@@ -71,6 +77,41 @@ div#head-search {
 div#google_translate_element {
     top: 10px;
 }
+</style>
+
+<?php else: ?>
+<style>
+  table.gsc-search-box td.gsc-clear-button {
+     display: none; 
+	}
+  td.gsc-search-button{
+    position: relative; 
+  }
+  input.gsc-search-button{
+    position: relative; 
+  }
+  span.search-icon{
+    right: 63px; 
+  }
+  input.gsc-search-button {
+    position: absolute;
+    top: 2px;
+    height: 51px;
+    width: 48px;
+    min-width: 35px ;
+    right: 30px;
+    opacity: 0;
+}
+  input.gsc-input {
+    background-image: unset !important;
+    padding: 14px;
+    height: 55px;
+    border: 3px solid transparent;
+    font-size: 1.1rem !important;
+}
+  input.gsc-input:focus {
+    border-color: transparent;
+  }
 </style>
 
 <?php endif; ?>
