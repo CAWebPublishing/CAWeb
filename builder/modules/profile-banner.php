540756b47e56e5d567ac00d676ec797396aea20c
Danny Guzman <guzmanjd86@gmail.com>
2018-06-18 12:19:48 -0700

    CAWeb v1.4.2

--- builder/modules/profile-banner.php
+++ builder/modules/profile-banner.php
@@ -4,16 +4,16 @@ Divi Icon Field Names
 When using the et_pb_get_font_icon_list to render the icon picker,
 make sure the field name is one of the following:
 'font_icon', 'button_one_icon', 'button_two_icon',  'button_icon'
-*/
+ */
 
-class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module{
-	function init() {
-		$this->name = esc_html__( 'Profile Banner', 'et_builder' );
+class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module {
+    function init() {
+        $this->name = esc_html__('Profile Banner', 'et_builder');
 
-		$this->slug = 'et_pb_profile_banner';
-		$this->fb_support = true;
+        $this->slug = 'et_pb_profile_banner';
+        $this->fb_support = true;
 
-		$this->whitelisted_fields = array(
+        $this->whitelisted_fields = array(
 			'name',
 			'max_width',
 			'max_width_tablet',
@@ -30,29 +30,29 @@ class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module{
 			'round_image',
 		);
 
-		$this->fields_defaults = array(
-			'url'       => array('#','add_default_setting'),
+        $this->fields_defaults = array(
+			'url'       => array('#', 'add_default_setting'),
 		);
 
-		$this->main_css_element = '%%order_class%%.et_pb_profile_banner';
+        $this->main_css_element = '%%order_class%%.et_pb_profile_banner';
 
-		$this->options_toggles = array(
+        $this->options_toggles = array(
 		  'general' => array(
 		    'toggles' => array(
-		      'style'  => esc_html__( 'Style', 'et_builder'),
-		      'header' => esc_html__( 'Header', 'et_builder'),
-		      'body'   => esc_html__( 'Body', 'et_builder'),
+		      'style'  => esc_html__('Style', 'et_builder'),
+		      'header' => esc_html__('Header', 'et_builder'),
+		      'body'   => esc_html__('Body', 'et_builder'),
 		    ),
 		  ),
 		  'advanced' => array(
 		    'toggles' => array(
-		      'body'   => esc_html__( 'Body', 'et_builder'),
+		      'body'   => esc_html__('Body', 'et_builder'),
 		      'text' => array(
-		        'title'    => esc_html__( 'Text', 'et_builder' ),
+		        'title'    => esc_html__('Text', 'et_builder'),
 		        'priority' => 49,
 		      ),
 		      'width' => array(
-		        'title'    => esc_html__( 'Sizing', 'et_builder' ),
+		        'title'    => esc_html__('Sizing', 'et_builder'),
 		        'priority' => 65,
 		      ),
 		    ),
@@ -63,63 +63,63 @@ class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module{
 		  ),
 		);
 
-		// Custom handler: Output JS for editor preview in page footer.
-		add_action( 'wp_footer', array($this, 'js_frontend_preview') );
-	}
-	function get_fields() {
-		$fields = array(
+        // Custom handler: Output JS for editor preview in page footer.
+		//add_action( 'wp_footer', array($this, 'js_frontend_preview') );
+    }
+    function get_fields() {
+        $fields = array(
 			'name' => array(
-				'label'           => esc_html__( 'Profile Name', 'et_builder' ),
+				'label'           => esc_html__('Profile Name', 'et_builder'),
 				'type'            => 'text',
 				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Input the name of the profile.', 'et_builder' ),
+				'description'     => esc_html__('Input the name of the profile.', 'et_builder'),
 				'toggle_slug'			=> 'header',
 			),
 			'job_title' => array(
-				'label'           => esc_html__( 'Job Title', 'et_builder' ),
+				'label'           => esc_html__('Job Title', 'et_builder'),
 				'type'            => 'text',
 				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Input the job title.', 'et_builder' ),
+				'description'     => esc_html__('Input the job title.', 'et_builder'),
 				'toggle_slug'			=> 'header',
 			),
 			'profile_link' => array(
-				'label'           => esc_html__( 'Profile Link', 'et_builder' ),
+				'label'           => esc_html__('Profile Link', 'et_builder'),
 				'type'            => 'text',
 				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Input the text for the profile link.', 'et_builder' ),
+				'description'     => esc_html__('Input the text for the profile link.', 'et_builder'),
 				'toggle_slug'			=> 'body',
 			),
 			'url' => array(
-				'label'           => esc_html__( 'Profile URL', 'et_builder' ),
+				'label'           => esc_html__('Profile URL', 'et_builder'),
 				'type'            => 'text',
 				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Input the website of the profile (http:// must be included).', 'et_builder' ),
+				'description'     => esc_html__('Input the website of the profile (http:// must be included).', 'et_builder'),
 				'toggle_slug'			=> 'body',
 			),
 			'portrait_url' => array(
-				'label'              => esc_html__( 'Portrait Image URL', 'et_builder' ),
+				'label'              => esc_html__('Portrait Image URL', 'et_builder'),
 				'type'               => 'upload',
 				'option_category'    => 'basic_option',
-				'upload_button_text' => esc_attr__( 'Upload an image', 'et_builder' ),
-				'choose_text'        => esc_attr__( 'Choose an Image', 'et_builder' ),
-				'update_text'        => esc_attr__( 'Set As Image', 'et_builder' ),
-				'description'        => esc_html__( 'Upload your desired image, or type in the URL to the image you would like to display. (http:// must be included)', 'et_builder' ),
+				'upload_button_text' => esc_attr__('Upload an image', 'et_builder'),
+				'choose_text'        => esc_attr__('Choose an Image', 'et_builder'),
+				'update_text'        => esc_attr__('Set As Image', 'et_builder'),
+				'description'        => esc_html__('Upload your desired image, or type in the URL to the image you would like to display. (http:// must be included)', 'et_builder'),
 				'toggle_slug'			=> 'body',
 			),
 			'round_image' => array(
-				'label'              => esc_html__( 'Round Image', 'et_builder' ),
+				'label'              => esc_html__('Round Image', 'et_builder'),
 				'type'               => 'yes_no_button',
 				'option_category'    => 'configuration',
 				'options'        => array(
-				  'off' => esc_html__( 'No', 'et_builder' ),
-				  'on'  => esc_html__( 'Yes', 'et_builder' ),
+				  'off' => esc_html__('No', 'et_builder'),
+				  'on'  => esc_html__('Yes', 'et_builder'),
 				),
 			  'description' => esc_html__('Switch to yes if you want round images in the profile banner.'),
 				'toggle_slug'			=> 'body',
 				'tab_slug'			=> 'advanced',
 			),
 			'max_width' => array(
-			  'label'           => esc_html__( 'Max Width', 'et_builder' ),
+			  'label'           => esc_html__('Max Width', 'et_builder'),
 			  'type'            => 'skip',
 			  'option_category' => 'layout',
 			  'mobile_options'  => true,
@@ -143,21 +143,21 @@ class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module{
 			  'toggle_slug' => 'width',
 			),
 			'disabled_on' => array(
-			  'label'           => esc_html__( 'Disable on', 'et_builder' ),
+			  'label'           => esc_html__('Disable on', 'et_builder'),
 			  'type'            => 'multiple_checkboxes',
 			  'options'         => array(
-			    'phone'   => esc_html__( 'Phone', 'et_builder' ),
-			    'tablet'  => esc_html__( 'Tablet', 'et_builder' ),
-			    'desktop' => esc_html__( 'Desktop', 'et_builder' ),
+			    'phone'   => esc_html__('Phone', 'et_builder'),
+			    'tablet'  => esc_html__('Tablet', 'et_builder'),
+			    'desktop' => esc_html__('Desktop', 'et_builder'),
 			  ),
 			  'additional_att'  => 'disable_on',
 			  'option_category' => 'configuration',
-			  'description'     => esc_html__( 'This will disable the module on selected devices', 'et_builder' ),
+			  'description'     => esc_html__('This will disable the module on selected devices', 'et_builder'),
 				'tab_slug'        => 'custom_css',
 				'toggle_slug'     => 'visibility',
 			),
 			'module_id' => array(
-			  'label'           => esc_html__( 'CSS ID', 'et_builder' ),
+			  'label'           => esc_html__('CSS ID', 'et_builder'),
 			  'type'            => 'text',
 			  'option_category' => 'configuration',
 			  'tab_slug'        => 'custom_css',
@@ -165,7 +165,7 @@ class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module{
 			  'option_class'    => 'et_pb_custom_css_regular',
 			),
 			'module_class' => array(
-			  'label'           => esc_html__( 'CSS Class', 'et_builder' ),
+			  'label'           => esc_html__('CSS Class', 'et_builder'),
 			  'type'            => 'text',
 			  'option_category' => 'configuration',
 			  'tab_slug'        => 'custom_css',
@@ -173,73 +173,71 @@ class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module{
 			  'option_class'    => 'et_pb_custom_css_regular',
 			),
 			'admin_label' => array(
-			  'label'       => esc_html__( 'Admin Label', 'et_builder' ),
+			  'label'       => esc_html__('Admin Label', 'et_builder'),
 			  'type'        => 'text',
-			  'description' => esc_html__( 'This will change the label of the module in the builder for easy identification.', 'et_builder' ),
+			  'description' => esc_html__('This will change the label of the module in the builder for easy identification.', 'et_builder'),
 				'toggle_slug'	=> 'admin_label',
 			),
 		);
 
-		return $fields;
+        return $fields;
+    }
+    function shortcode_callback($atts, $content = null, $function_name) {
+        $module_id            = $this->shortcode_atts['module_id'];
+        $module_class         = $this->shortcode_atts['module_class'];
+        $name                 = $this->shortcode_atts['name'];
+        $job_title              = $this->shortcode_atts['job_title'];
+        $profile_link              = $this->shortcode_atts['profile_link'];
+        $portrait_url           = $this->shortcode_atts['portrait_url'];
+        $round                = $this->shortcode_atts['round_image'];
+        $url                    = $this->shortcode_atts['url'];
+        $max_width            = $this->shortcode_atts['max_width'];
+        $max_width_tablet     = $this->shortcode_atts['max_width_tablet'];
+        $max_width_phone      = $this->shortcode_atts['max_width_phone'];
+        $max_width_last_edited = $this->shortcode_atts['max_width_last_edited'];
 
-	}
-	function shortcode_callback($atts, $content = null, $function_name) {
-		$module_id            = $this->shortcode_atts['module_id'];
-		$module_class         = $this->shortcode_atts['module_class'];
-		$name                 = $this->shortcode_atts['name'];
-		$job_title              = $this->shortcode_atts['job_title'];
-		$profile_link              = $this->shortcode_atts['profile_link'];
-		$portrait_url           = $this->shortcode_atts['portrait_url'];
-		$round                = $this->shortcode_atts['round_image'];
-		$url                    = $this->shortcode_atts['url'];
-		$max_width            = $this->shortcode_atts['max_width'];
-		$max_width_tablet     = $this->shortcode_atts['max_width_tablet'];
-		$max_width_phone      = $this->shortcode_atts['max_width_phone'];
-		$max_width_last_edited = $this->shortcode_atts['max_width_last_edited'];
+        $class = "et_pb_profile_banner et_pb_module";
+        $module_class = ET_Builder_Element::add_module_order_class($module_class, $function_name);
 
-		$class = "et_pb_profile_banner et_pb_module";
-		$module_class = ET_Builder_Element::add_module_order_class( $module_class, $function_name );
+        if ('' !== $max_width_tablet || '' !== $max_width_phone || '' !== $max_width) {
+            $max_width_responsive_active = et_pb_get_responsive_status($max_width_last_edited);
 
-		if ( '' !== $max_width_tablet || '' !== $max_width_phone || '' !== $max_width ) {
-		  $max_width_responsive_active = et_pb_get_responsive_status( $max_width_last_edited );
-
-		  $max_width_values = array(
+            $max_width_values = array(
 		    'desktop' => $max_width,
 		    'tablet'  => $max_width_responsive_active ? $max_width_tablet : '',
 		    'phone'   => $max_width_responsive_active ? $max_width_phone : '',
 		  );
 
-		  et_pb_generate_responsive_css( $max_width_values, '%%order_class%%', 'max-width', $function_name );
-		}
+            et_pb_generate_responsive_css($max_width_values, '%%order_class%%', 'max-width', $function_name);
+        }
 
-		$image = ('on' !== $round ?
+        $image = ('on' !== $round ?
 						sprintf('<img src="%1$s" style="width: 90px; min-height: 90px;float: right;"/>', $portrait_url) :
 						sprintf('<div class="profile-banner-img-wrapper">
 							<img src="%1$s" style="width: 90px; min-height: 90px;float: right;"/>
 						</div>', $portrait_url)
 				  );
 
-		$output = sprintf('<div id="profile-banner-wrapper" class="%7$s%8$s"><a href="%4$s"><div class="profile-banner%6$s">
+        $output = sprintf('<div id="profile-banner-wrapper" class="%7$s%8$s"><a href="%4$s"><div class="profile-banner%6$s">
 											%1$s
 											<div class="banner-subtitle">%2$s</div>
 											<div class="banner-title">%3$s</div>
 											<div class="banner-link"><p>%5$s</p>
 						          </div></div></a></div>',
 											$image, $job_title, $name, $url,
-					$profile_link, ('on' !== $round ? '' : ' round-image'),  esc_attr( $class ),
-    	    ( '' !== $module_class ? sprintf( ' %1$s', esc_attr( $module_class ) ) : '' ) );
+					$profile_link, ('on' !== $round ? '' : ' round-image'),  esc_attr($class),
+    	    ('' !== $module_class ? sprintf(' %1$s', esc_attr($module_class)) : ''));
 
-		return $output;
+        return $output;
+    }
 
-	}
-
-		// This is a non-standard function. It outputs JS code to render the
-		// module preview in the new Divi 3 frontend editor.
-		// Return value of the JS function must be full HTML code to display.
-		function js_frontend_preview() {
-			?>
+    // This is a non-standard function. It outputs JS code to render the
+    // module preview in the new Divi 3 frontend editor.
+    // Return value of the JS function must be full HTML code to display.
+    function js_frontend_preview() {
+        ?>
 			<script>
-			window.<?php echo $this->slug; ?>_preview = function(args) {
+			window.<?php print $this->slug; ?>_preview = function(args) {
 				var output = '';
 
 				var image = ('on' !== args.round_image ?
@@ -254,7 +252,7 @@ class ET_Builder_Module_Profile_Banner extends ET_Builder_CAWeb_Module{
 			}
 			</script>
 			<?php
-		}
+    }
 }
 new ET_Builder_Module_Profile_Banner;
 
--- includes/filters.php
+++ includes/filters.php
@@ -2,11 +2,11 @@
 
 // TinyMCE Editor
 // Attach callback to 'tiny_mce_before_init'
-add_filter( 'tiny_mce_before_init', 'caweb_tiny_mce_before_init' );
+add_filter('tiny_mce_before_init', 'caweb_tiny_mce_before_init', 15);
 function caweb_tiny_mce_before_init($init_array) {
-	// Define the style_formats array
-	// Each array child is a format with it's own settings
-	$style_formats = array(
+    // Define the style_formats array
+    // Each array child is a format with it's own settings
+    $style_formats = array(
 		array(
 			'title' => 'Featured Narrative',
 			'block' => 'aside',
@@ -43,105 +43,103 @@ function caweb_tiny_mce_before_init($init_array) {
 		),
 	);
 
-	// Insert the array, JSON ENCODED, into 'style_formats'
-  $init_array['style_formats'] = json_encode( $style_formats );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode($style_formats);
 
-  // TinyMCE Toolbar Start off unhidden
-	$init_array['wordpress_adv_hidden'] = false;
+    // TinyMCE Toolbar Start off unhidden
+    $init_array['wordpress_adv_hidden'] = false;
 
-  return $init_array;
-
+    return $init_array;
 }
 
 // Add hidden MCE Buttons
 // The primary toolbar (always visible)
-add_filter( 'mce_buttons', 'caweb_mce_buttons' );
+add_filter('mce_buttons', 'caweb_mce_buttons', 15);
 function caweb_mce_buttons($buttons) {
-	/**
-		Add in a core button that's disabled by default
-	**/
-  $tmp = array('formatselect', 'bold', 'italic', 'underline');
-  array_splice($buttons, 0, 3, $tmp);
+    /**
+     * Add in a core button that's disabled by default
+     **/
+    $tmp = array('formatselect', 'bold', 'italic', 'underline');
+    array_splice($buttons, 0, 3, $tmp);
 
-	return $buttons;
-
+    return $buttons;
 }
 
-add_filter( 'mce_buttons_2', 'caweb_mce_buttons_2' );
+add_filter('mce_buttons_2', 'caweb_mce_buttons_2', 15);
 function caweb_mce_buttons_2($buttons) {
 
 	/**
-		Add in a core button that's disabled by default
-	**/
+	 * Add in a core button that's disabled by default
+	 **/
 
-	$tmp = array('styleselect', 'strikethrough', 'hr', 'fontselect', 'fontsizeselect',
-               'forecolor', 'backcolor', 'pastetext', 'copy','subscript', 'superscript');
-  array_splice($buttons, 0, 5, $tmp);
+    $tmp = array('styleselect', 'strikethrough', 'hr', 'fontselect', 'fontsizeselect',
+               'forecolor', 'backcolor', 'pastetext', 'copy', 'subscript', 'superscript');
+    array_splice($buttons, 0, 5, $tmp);
 
-	return $buttons;
-
+    return $buttons;
 }
 
 // CAWeb Page Body Class
-add_filter( 'body_class', 'caweb_body_class', 20, 2 );
+add_filter('body_class', 'caweb_body_class', 20, 2);
 function caweb_body_class($wp_classes, $extra_classes) {
-	global $post;
+    global $post;
 
-	// List of the classes that need to be removed
-	$blacklist= array('et_secondary_nav_dropdown_animation_fade',
+    // List of the classes that need to be removed
+    $blacklist= array('et_secondary_nav_dropdown_animation_fade',
 				'et_primary_nav_dropdown_animation_fade', 'et_fixed_nav', 'et_show_nav', 'et_right_sidebar');
 
-	// List of extra classes that need to be added to the body
-	if( isset($post->ID) ){
-		$divi = et_pb_is_pagebuilder_used( $post->ID );
-		$sidebar_enabled = ! is_page();
-		$special_templates = is_tag() || is_archive() || is_category() || is_author();
+    // List of extra classes that need to be added to the body
+    if (isset($post->ID)) {
+        $divi = et_pb_is_pagebuilder_used($post->ID);
+        $sidebar_enabled = ! is_page();
+        $special_templates = is_tag() || is_archive() || is_category() || is_author();
 
-		$whitelist = array((  $divi && ! $special_templates ?  'divi_builder' : 'non_divi_builder' ),
-                     (  "on" == get_post_meta($post->ID, 'ca_custom_post_title_display', true) ? 'title_displayed' : 'title_not_displayed' ),
-                      sprintf('v%1$s', caweb_get_version($post->ID) ),
-                       (is_active_sidebar('sidebar-1') && $sidebar_enabled  ? 'sidebar_displayed' : 'sidebar_not_displayed'  ));
-	}
-	$whitelist[] = ( get_option('ca_sticky_navigation') ?  'sticky_nav' : '');
+        $whitelist = array(($divi && ! $special_templates ? 'divi_builder' : 'non_divi_builder'),
+                     ("on" == get_post_meta($post->ID, 'ca_custom_post_title_display', true) ? 'title_displayed' : 'title_not_displayed'),
+                      sprintf('v%1$s', caweb_get_version($post->ID)),
+                       (is_active_sidebar('sidebar-1') && $sidebar_enabled ? 'sidebar_displayed' : 'sidebar_not_displayed'));
+    }
+    $whitelist[] = (get_option('ca_sticky_navigation') ? 'sticky_nav' : '');
 
-   	// Remove any classes in the blacklist from the wp_classes
-	$wp_classes = array_diff( $wp_classes, $blacklist);
+    // Remove any classes in the blacklist from the wp_classes
+    $wp_classes = array_diff($wp_classes, $blacklist);
 
-	// Return filtered wp class
-	return  array_merge($wp_classes, (array) $whitelist);
-
+    // Return filtered wp class
+    return  array_merge($wp_classes, (array) $whitelist);
 }
 
 // CAWeb Post Body Class
-add_filter( 'post_class', 'caweb_post_class', 15 );
+add_filter('post_class', 'caweb_post_class', 15);
 function caweb_post_class($classes) {
-	global $post;
+    global $post;
 
-  if( has_post_thumbnail( $post->ID ) && "" == get_the_post_thumbnail_url( $post->ID ) )
-		 unset( $classes[ array_search("has-post-thumbnail", $classes) ] );
+    if (has_post_thumbnail($post->ID) && "" == get_the_post_thumbnail_url($post->ID)) {
+        unset($classes[ array_search("has-post-thumbnail", $classes) ]);
+    }
 
-	return $classes;
+    return $classes;
 }
 
 // CAWeb Theme Page Templates
-add_filter( 'theme_page_templates', 'caweb_theme_page_templates' );
+add_filter('theme_page_templates', 'caweb_theme_page_templates');
 function caweb_theme_page_templates($templates) {
-	// Remove Divi Blank Page Template
-    unset( $templates['page-template-blank.php'] );
+    // Remove Divi Blank Page Template
+    unset($templates['page-template-blank.php']);
+
     return $templates;
 }
 
 // CAWeb Script Loader Tags
 add_filter('script_loader_tag', 'caweb_script_loader_tag', 10, 3);
 function caweb_script_loader_tag($tag, $handle, $src) {
-	// Defer some scripts
-	$js_scripts = array('cagov-modernizr-script', 'cagov-modernizr-extra-script', 'cagov-navigation-script', 'cagov-ga-autotracker-script', 'cagov-google-script', 'thickbox');
-	  // deferring jQuery breaks other scripts preg_match('/(jquery)[^\/]*\.js/', $tag)
-	  if( in_array($handle, $js_scripts) )
-		  return str_replace('src', 'defer src', $tag);
+    // Defer some scripts
+    $js_scripts = array('cagov-modernizr-script', 'cagov-modernizr-extra-script', 'cagov-navigation-script', 'cagov-ga-autotracker-script', 'cagov-google-script', 'thickbox');
+    // deferring jQuery breaks other scripts preg_match('/(jquery)[^\/]*\.js/', $tag)
+    if (in_array($handle, $js_scripts)) {
+        return str_replace('src', 'defer src', $tag);
+    }
 
-  return $tag;
-
+    return $tag;
 }
 
 ?>
