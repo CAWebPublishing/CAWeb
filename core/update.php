69c392e158f46f7869c79d2aae795fe5823f135a
Danny Guzman <guzmanjd86@gmail.com>
2018-11-01 10:39:12 -0700

    Update update.php
    Updated version_compare sanity check for update payload

--- core/update.php
+++ core/update.php
@@ -112,8 +112,8 @@ if ( ! class_exists('CAWeb_Theme_Update')) {
             if ( ! is_wp_error($payload) && wp_remote_retrieve_response_code($payload) == 200) {
                 $payload = json_decode(wp_remote_retrieve_body($payload));
 
-                //version_compare( $payload->tag_name,  $this->current_version, '>')
-                if ( ! empty($payload) &&  ($payload->tag_name >  $this->current_version)) {
+                 //  ($payload->tag_name >  $this->current_version)
+                if ( ! empty($payload) && version_compare( $payload->tag_name,  $this->current_version, '>')) {
                     $obj = array();
                     $obj['new_version'] = $payload->tag_name;
 
