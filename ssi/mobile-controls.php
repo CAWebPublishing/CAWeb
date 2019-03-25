80ee2b56af6b163fab0bdeafab86d7f21d26df64
Danny Guzman <guzmanjd86@gmail.com>
2019-03-25 16:39:38 -0700

    Added missing V5.0.2 styles Update Mobile Controls

--- ssi/mobile-controls.php
+++ ssi/mobile-controls.php
@@ -2,7 +2,11 @@
 <div class="mobile-controls">
 		<div class="mobile-control-group main-nav-icons pull-right">
 			<button class="mobile-control toggle-menu pull-right" aria-expanded="false" aria-controls="navigation" data-toggle="collapse" data-target="#navigation">
-				<span class="ca-gov-icon-menu hidden-print" aria-hidden="true"></span><span class="sr-only">Menu</span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span class="sr-only">Menu</span>
 			</button>
 			<button class="mobile-control toggle-search">
 				<span class="ca-gov-icon-search hidden-print" aria-hidden="true"></span><span class="sr-only">Search</span>
