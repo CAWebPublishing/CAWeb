cb40fab1760ec6c54fac41b63f46b0ef1a3448c9
Danny Guzman <guzmanjd86@gmail.com>
2019-04-23 14:03:36 -0700

    Added custom JS to move translate from one location to another during mobile mode transition

--- js/wplibs/custom.js
+++ js/wplibs/custom.js
@@ -1,5 +1,28 @@
  jQuery(document).ready(function() {
 	 $('.caweb-alert-close').click( function(e){ jQuery.post(this.dataset.url); });
 	 
-	 
+	// run test on initial page load
+	checkSize();
+
+	// run test on resize of the window
+	$(window).resize(checkSize);
  });
+
+ function checkSize(){
+	var utility_container = $('.global-header .utility-header .container');
+	var translate = utility_container.find('#google_translate_element');
+	var row = document.createElement('DIV');
+
+	row.className = "group flex-row";
+
+	// If mobile controls are visible
+    if ( 1 === utility_container.children().length && "none" !== $(".global-header .mobile-controls").css("display") ){
+		row.append(translate[0]);
+		utility_container.append(row);
+	// If mobile controls are not visible
+    }else if(2 === utility_container.children().length && "none" === $(".global-header .mobile-controls").css("display") ) {
+		utility_container.children()[0].append(translate[0]);
+		utility_container.children()[1].remove();
+
+	}
+}
