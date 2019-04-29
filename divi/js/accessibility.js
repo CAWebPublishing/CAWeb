eb7778050aa2cb1cc588c765e18e432fb8d5e8b7
Danny Guzman <guzmanjd86@gmail.com>
2019-04-29 09:56:28 -0700

    Tab module role had an uppercase letter 'tabList' was changed to 'tablist'

--- divi/js/accessibility.js
+++ divi/js/accessibility.js
@@ -2,22 +2,43 @@
 
    jQuery(document).ready(function() {
     
-    var blogs = $('div').filter(function(){ return this.className.match(/et_pb_blog_\d/); });
+    /* Divi Blog Module Accessibility */
+    // Retrieve all Divi Blog Modules
+    var blog_modules = $('div').filter(function(){ return this.className.match(/et_pb_blog_\d/); });
 
-    if( blogs.length ){
-        blogs.each(function(index, element) {
+    // Run only if there is a Blog Module on the current page
+    if( blog_modules.length ){
+        blog_modules.each(function(index, element) {
+            // Grab each blog article
             blog =  $(element).find('article');
             blog.each(function(i) {
              b =  $(blog[i]); 
+             // Grab the article title
              title = b.children('.entry-title').text();
              
+             // Grab the More Information Button from the Post content
+             // Divi appends the More Information button as the last child of the content
              read_more = b.children('.post-content').children('.more-link:last-child');
       
+             // If there is a More Information Button append SR Tag with Title
              if(read_more.length){
                  read_more.append('<span class="sr-only">' + title + '</span>');
              }
-             });
+            });
          });      
     }   
 
+    /* Divi Tab Module Accessibility */
+    // Retrieve all Divi Tab Modules
+    var tab_modules = $('div').filter(function(){ return this.className.match(/et_pb_tabs_\d/); });
+
+    // Run only if there is a Tab Module on the current page
+    if( tab_modules.length ){        
+        tab_modules.each(function(index, element) {
+            // Grab each tab control
+            tab_list =  $(element).find('ul.et_pb_tabs_controls');
+
+            tab_list.role = tab_list.role.toLowerCase(); 
+        });      
+    }   
 });
