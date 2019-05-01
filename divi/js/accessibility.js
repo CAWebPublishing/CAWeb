9b692d934038ac8947a7e7bb5dd108bcb163ea39
Danny Guzman <guzmanjd86@gmail.com>
2019-05-01 14:55:11 -0700

    Added no-underline class to Divi Slide Module More Button and Divi slider arrows

--- divi/js/accessibility.js
+++ divi/js/accessibility.js
@@ -10,13 +10,26 @@
     // Retrieve all Divi Tab Modules
     var tab_modules = $('div').filter(function(){ return this.className.match(/\bet_pb_tabs_\d\b/); });
 
-     /* Divi Image Module (Standard & Fullwidth) Accessibility */
+    /* Divi Image Module (Standard & Fullwidth) Accessibility */
     // Retrieve all Divi Image Modules
     var image_modules = $('div').filter(function(){ return this.className.match(/\bet_pb_image_\d\b|\bet_pb_fullwidth_image_\d\b/); });
 
-     /* Divi Slider Module (Standard & Fullwidth) Accessibility */
-    // Retrieve all Divi Image Modules
+    /* Divi Button Module Accessibility */
+    // Retrieve all Divi Button Modules
     var button_modules = $('a').filter(function(){ return this.className.match(/\bet_pb_button_\d\b/); });
+
+    /* Divi Slides (Standard & Fullwidth) Accessibility 
+        Slide Module is a child module used in the following modules:
+        Slider (Standard & Fullwidth)
+        Post Slider (Standard & Fullwidth)
+       Retrieve all Divi Slide Modules
+    */
+    var slide_modules = $('div').filter(function(){ return this.className.match(/\bet_pb_slide\b/); });
+    
+    /* Divi Slider Arrows Accessibility 
+       Retrieve all Divi Slider Arrows
+    */
+   var slider_arrows = $('div').filter(function(){ return this.className.match(/\bet-pb-slider-arrows\b/); });
 
     // Run only if there is a Blog Module on the current page
     if( blog_modules.length ){
@@ -96,4 +109,27 @@
          });      
     } 
 
+    // Run only if there is a Slide Module on the current page
+    if( slide_modules.length ){
+        slide_modules.each(function(index, element) {
+            // Grab each more button control
+            var more_button =  $(element).find('a.et_pb_more_button');
+
+            more_button.addClass('no-underline');
+            
+         });      
+    } 
+
+    // Run only if there are Slide Arrows on the current page
+    if( slider_arrows.length ){
+        slider_arrows.each(function(index, element) {
+            // Grab each more button control
+            var prev_button =  $(element).find('a.et-pb-arrow-prev');
+            var next_button =  $(element).find('a.et-pb-arrow-next');
+
+            prev_button.addClass('no-underline');
+            next_button.addClass('no-underline');
+            
+        });      
+    } 
 });
