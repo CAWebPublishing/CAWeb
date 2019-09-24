25b2b0aa5fb5e6a5b406f6c897f323d485944699
Danny Guzman <guzmanjd86@gmail.com>
2019-09-24 13:50:43 -0700

    Corrected Contrast ADA issue with The Event Calendar Plugin

--- js/wplibs/accessibility.js
+++ js/wplibs/accessibility.js
@@ -198,15 +198,51 @@ jQuery(document).ready(function() {
        /* 
         The Events Calendar Accessibility 
         */
-       var event_calendar_element = $('#tribe-bar-form span[role="none"], #tribe-bar-form li[role="option"]');
+       var event_calendar_form_element = $('#tribe-bar-form span[role="none"], #tribe-bar-form li[role="option"]');
             
-       if( event_calendar_element.length ){
-            event_calendar_element.each(function(index, element) {
+       if( event_calendar_form_element.length ){
+        event_calendar_form_element.each(function(index, element) {
                 $(element).removeAttr('role', '');
             });
        }
 
+       var event_calendar_element = $('.tribe-events-calendar');
+            
+       if( event_calendar_element.length ){
+            event_calendar_element.each(function(index, element) {
+                var th = $(element).find('thead tr th');
+                var future_dates = $(element).find('tbody tr td.tribe-events-thismonth.tribe-events-future div');
+                var past_dates = $(element).find('tbody tr td.tribe-events-thismonth.tribe-events-past div');
+                
+                // Tribe Event Display Contrast Fixes
+                if( "#666666" == rgb2hex( $(th[0]).css( "background-color" ) ) ){
+                    th.each(function(i, e){
+                        $(e).css( "background-color", "#dddddd" );
+                    });
+
+                    future_dates.each(function(i,e){
+                        $(e).css( "background-color", "#f7f7f7" );
+                        $(e).css("color", "#707070");
+                    });
+                
+                // Full Style Display Contrast Fixes
+                }else if( "#dddddd" == rgb2hex( $(th[0]).css( "background-color" )) ){
+                    past_dates.each(function(i,e){
+                        $(e).css("color", "#333333");
+                    });
+                }
+            });
+       }
+       
     }); // End of window load
+
+    function rgb2hex(rgb){
+        rgb = rgb.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+        return "#" +
+         ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+         ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+         ("0" + parseInt(rgb[3],10).toString(16)).slice(-2);
+       }
 
     function stripeIframeAttributes(frame){
         $(frame).removeAttr('frameborder', '');
