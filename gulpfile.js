e3a744b4ef9e526e71d008b79ecc7aa7b8000ccb
Danny Guzman <guzmanjd86@gmail.com>
2019-10-11 15:27:36 -0700

    Continuing to configure Gulp

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+/**
+ * Gulpfile.
+ *
+ * Gulp with WordPress.
+ *
+ * Implements:
+ *      1. Live reloads browser with BrowserSync.
+ *      2. CSS: Sass to CSS conversion, error catching, Autoprefixing, Sourcemaps,
+ *         CSS minification, and Merge Media Queries.
+ *      3. JS: Concatenates & uglifies Vendor and Custom JS files.
+ *      4. Images: Minifies PNG, JPEG, GIF and SVG images.
+ *      5. Watches files for changes in CSS or JS.
+ *      6. Watches files for changes in PHP.
+ *      7. Corrects the line endings.
+ *      8. InjectCSS instead of browser page reload.
+ *      9. Generates .pot file for i18n and l10n.
+ *
+ * @tutorial https://github.com/ahmadawais/WPGulp
+ * @author Ahmad Awais <https://twitter.com/MrAhmadAwais/>
+ */
+
+/**
+ * Load WPGulp Configuration.
+ *
+ * TODO: Customize your project in the wpgulp.js file.
+ */
+const config = require( './wpgulp.config.js' );
+
+/**
+ * Load Plugins.
+ *
+ * Load gulp plugins and passing them semantic names.
+ */
+const gulp = require( 'gulp' ); // Gulp of-course.
+
+// CSS related plugins.
+const sass = require( 'gulp-sass' ); // Gulp plugin for Sass compilation.
+const minifycss = require( 'gulp-uglifycss' ); // Minifies CSS files.
+const autoprefixer = require( 'gulp-autoprefixer' ); // Autoprefixing magic.
+const mmq = require( 'gulp-merge-media-queries' ); // Combine matching media queries into one.
+const rtlcss = require( 'gulp-rtlcss' ); // Generates RTL stylesheet.
+
+// JS related plugins.
+const concat = require( 'gulp-concat' ); // Concatenates JS files.
+const uglify = require( 'gulp-uglify' ); // Minifies JS files.
+const babel = require( 'gulp-babel' ); // Compiles ESNext to browser compatible JS.
+
+// Image related plugins.
+const imagemin = require( 'gulp-imagemin' ); // Minify PNG, JPEG, GIF and SVG images with imagemin.
+
+// Utility related plugins.
+const rename = require( 'gulp-rename' ); // Renames files E.g. style.css -> style.min.css.
+const lineec = require( 'gulp-line-ending-corrector' ); // Consistent Line Endings for non UNIX systems. Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings).
+const filter = require( 'gulp-filter' ); // Enables you to work on a subset of the original files by filtering them using a glob.
+const sourcemaps = require( 'gulp-sourcemaps' ); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css).
+const notify = require( 'gulp-notify' ); // Sends message notification to you.
+const browserSync = require( 'browser-sync' ).create(); // Reloads browser and injects CSS. Time-saving synchronized browser testing.
+const wpPot = require( 'gulp-wp-pot' ); // For generating the .pot file.
+const sort = require( 'gulp-sort' ); // Recommended to prevent unnecessary changes in pot-file.
+const cache = require( 'gulp-cache' ); // Cache files in stream for later use.
+const remember = require( 'gulp-remember' ); //  Adds all the files it has ever seen back into the stream.
+const plumber = require( 'gulp-plumber' ); // Prevent pipe breaking caused by errors from gulp plugins.
+const beep = require( 'beepbeep' );
+
+const v4CSSFiles = [
+	'assets/css/cagov/cagov.font-only.css',
+	'assets/css/cagov/custom.css',
+	'assets/css/cagov/version4/custom.css'
+];
+
+const v5CSSFiles = [
+	'assets/css/cagov/cagov.font-only.css',
+	'assets/css/cagov/custom.css',
+	'assets/css/cagov/version5/custom.css'
+];
+
+/*
+	CAWeb Oceanside (State Template v5)
+*/
+gulp.task('v5oceanside', function () {
+	var cssFiles = [
+		'assets/css/cagov/version5/cagov.core.css',
+		'assets/css/cagov/version5/colorscheme/oceanside.css'
+	].concat(v5CSSFiles);
+
+    return gulp.src(cssFiles)
+		.pipe(
+			sass({
+				outputStyle: config.outputDevStyle,
+			})
+		)
+		.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
+		.pipe(concat('caweb-oceanside.css')) // compiled file
+        .pipe(gulp.dest('./css/version5/'))
+});
+//.pipe(minifycss()) // minify
+        
+/**
+ * Task: `styles`.
+ *
+ * Compiles Sass, Autoprefixes it and Minifies CSS.
+ *
+ * This task does the following:
+ *    1. Gets the source scss file
+ *    2. Compiles Sass to CSS
+ *    3. Writes Sourcemaps for it
+ *    4. Autoprefixes it and generates style.css
+ *    5. Renames the CSS file with suffix .min.css
+ *    6. Minifies the CSS file and generates style.min.css
+ *    7. Injects CSS or reloads the browser via browserSync
+ */
+gulp.task( 'styles', () => {
+	return gulp
+		.src( 'assets/css/cagov/version5/cagov.core.css', { allowEmpty: true })
+		.pipe(
+			sass({
+				errLogToConsole: config.errLogToConsole,
+				outputStyle: config.outputStyle,
+				precision: config.precision
+			})
+		)
+		.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
+        .pipe(concat('assets/css/cagov/version5/colorscheme/orange county.css')) // compiled file
+		.pipe( gulp.dest( config.styleDestination ) )
+		.pipe( notify({ message: '\n\n✅  ===> STYLES — completed!\n', onLast: true }) );
+});
+
+
+//
+// DEV (Development Output)
+//
+//gulp.task('dev', ['v5oceanside']);
+
+// PROD (Minified Output)
+//gulp.task('prod', []);
--- wpgulp.config.js
+++ wpgulp.config.js
@@ -7,6 +7,7 @@
  * @package WPGulp
  */
 
+
 module.exports = {
 
 	// Project options.
@@ -18,39 +19,9 @@ module.exports = {
 	// Style options.
 	styleSRC: './assets/scss/style.scss', // Path to main .scss file.
 	styleDestination: './', // Path to place the compiled CSS file. Default set to root folder.
-	outputStyle: 'compressed', // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
+	outputDevStyle: 'expanded', // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
+	outputProdStyle: 'compressed', // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
 	errLogToConsole: true,
 	precision: 10,
 
-	// JS Vendor options.
-	jsVendorSRC: './assets/js/vendor/*.js', // Path to JS vendor folder.
-	jsVendorDestination: './assets/js/', // Path to place the compiled JS vendors file.
-	jsVendorFile: 'vendor', // Compiled JS vendors file name. Default set to vendors i.e. vendors.js.
-
-	// JS Custom options.
-	jsCustomSRC: './assets/js/custom/*.js', // Path to JS custom scripts folder.
-	jsCustomDestination: './assets/js/', // Path to place the compiled JS custom scripts file.
-	jsCustomFile: 'custom', // Compiled JS custom file name. Default set to custom i.e. custom.js.
-
-	// Watch files paths.
-	watchStyles: ['./assets/scss/**/*.scss', './assets/scss/**/*.css'], // Path to all *.scss files inside css folder and inside them.
-	watchJsVendor: './assets/js/vendor/*.js', // Path to all vendor JS files.
-	watchJsCustom: './assets/js/custom/*.js', // Path to all custom JS files.
-	watchPhp: './**/*.php', // Path to all PHP files.
-
-	// Browsers you care about for autoprefixing. Browserlist https://github.com/ai/browserslist
-	// The following list is set as per WordPress requirements. Though, Feel free to change.
-	BROWSERS_LIST: [
-		'last 2 version',
-		'> 1%',
-		'ie >= 11',
-		'last 1 Android versions',
-		'last 1 ChromeAndroid versions',
-		'last 2 Chrome versions',
-		'last 2 Firefox versions',
-		'last 2 Safari versions',
-		'last 2 iOS versions',
-		'last 2 Edge versions',
-		'last 2 Opera versions'
-	]
 };
