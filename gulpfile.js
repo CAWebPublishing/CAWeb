763201c3b31f055ed68420c9e806213fe7f66e17
Danny Guzman <guzmanjd86@gmail.com>
2019-10-15 20:02:53 -0700

    Gulp configurations have been set up

--- gulpfile.js
+++ gulpfile.js
@@ -32,104 +32,177 @@ const config = require( './wpgulp.config.js' );
  * Load gulp plugins and passing them semantic names.
  */
 const gulp = require( 'gulp' ); // Gulp of-course.
+const parameterized = require('gulp-parameterized');
 
 // CSS related plugins.
 const sass = require( 'gulp-sass' ); // Gulp plugin for Sass compilation.
-const minifycss = require( 'gulp-uglifycss' ); // Minifies CSS files.
-const autoprefixer = require( 'gulp-autoprefixer' ); // Autoprefixing magic.
-const mmq = require( 'gulp-merge-media-queries' ); // Combine matching media queries into one.
-const rtlcss = require( 'gulp-rtlcss' ); // Generates RTL stylesheet.
-
-// JS related plugins.
-const concat = require( 'gulp-concat' ); // Concatenates JS files.
-const uglify = require( 'gulp-uglify' ); // Minifies JS files.
-const babel = require( 'gulp-babel' ); // Compiles ESNext to browser compatible JS.
 
 // Image related plugins.
 const imagemin = require( 'gulp-imagemin' ); // Minify PNG, JPEG, GIF and SVG images with imagemin.
 
 // Utility related plugins.
-const rename = require( 'gulp-rename' ); // Renames files E.g. style.css -> style.min.css.
+const concat = require( 'gulp-concat' ); // Concatenates files.
 const lineec = require( 'gulp-line-ending-corrector' ); // Consistent Line Endings for non UNIX systems. Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings).
-const filter = require( 'gulp-filter' ); // Enables you to work on a subset of the original files by filtering them using a glob.
-const sourcemaps = require( 'gulp-sourcemaps' ); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css).
-const notify = require( 'gulp-notify' ); // Sends message notification to you.
-const browserSync = require( 'browser-sync' ).create(); // Reloads browser and injects CSS. Time-saving synchronized browser testing.
-const wpPot = require( 'gulp-wp-pot' ); // For generating the .pot file.
-const sort = require( 'gulp-sort' ); // Recommended to prevent unnecessary changes in pot-file.
-const cache = require( 'gulp-cache' ); // Cache files in stream for later use.
-const remember = require( 'gulp-remember' ); //  Adds all the files it has ever seen back into the stream.
-const plumber = require( 'gulp-plumber' ); // Prevent pipe breaking caused by errors from gulp plugins.
-const beep = require( 'beepbeep' );
 
-const v4CSSFiles = [
-	'assets/css/cagov/cagov.font-only.css',
-	'assets/css/cagov/custom.css',
-	'assets/css/cagov/version4/custom.css'
-];
+const fs = require('fs'); // File System
 
-const v5CSSFiles = [
-	'assets/css/cagov/cagov.font-only.css',
-	'assets/css/cagov/custom.css',
-	'assets/css/cagov/version5/custom.css'
-];
+/* 
+	CAWeb Admin Styles 
+*/
+gulp.task('caweb-admin-styles', parameterized( async function (_) {
+	var noFlags = undefined === _.params.length || _.params.all;
+	
+	if ( _.params.prod ) {
+		buildAdminStyles(true);
+	}
+
+	if ( _.params.dev ) {
+		buildAdminStyles(false);
+	}	
+
+	if( noFlags ){
+		buildAdminStyles(false);
+	}
+}));
 
 /*
-	CAWeb Oceanside (State Template v5)
+	CAWeb Styles (State Template v5)
 */
-gulp.task('v5oceanside', function () {
-	var cssFiles = [
-		'assets/css/cagov/version5/cagov.core.css',
-		'assets/css/cagov/version5/colorscheme/oceanside.css'
-	].concat(v5CSSFiles);
+gulp.task('caweb-v5-styles', parameterized( async function (_) {
+	var noFlags = undefined === _.params.length || _.params.all;
 
-    return gulp.src(cssFiles)
+	if ( _.params.prod ) {
+		buildVersionStyles(true, '5');
+	}
+
+	if ( _.params.dev ) {
+		buildVersionStyles(false, '5');
+	}	
+
+	if( noFlags ){
+		buildVersionStyles(false, '5');
+		buildVersionStyles(true, '5');
+	}
+	
+}));
+
+/*
+	CAWeb Styles (State Template v4 Legacy Version)
+*/
+gulp.task('caweb-v4-styles', parameterized( async function (_) {
+	var noFlags = undefined === _.params.length || _.params.all;
+	
+	if ( _.params.prod ) {
+		buildVersionStyles(true, '4');
+	}
+
+	if ( _.params.dev ) {
+		buildVersionStyles(false, '4');
+	}	
+
+	if( noFlags ){
+		buildVersionStyles(false, '4');
+		buildVersionStyles(true, '4');
+	}
+}));
+
+gulp.task('build', parameterized(async function(_){
+	var noFlags = ! Object.getOwnPropertyNames(_.params).length || undefined !== _.params.all;
+	var versionNum = undefined !== _.params.ver ? _.params.ver : false; 
+	
+	if ( _.params.prod ) {
+		buildAdminStyles(true);
+
+		if( versionNum ){
+			buildVersionStyles(true, versionNum);
+		}else{
+			buildVersionStyles(true, '4');
+			buildVersionStyles(true, '5');
+		}
+	}
+
+	if ( _.params.dev ) {
+		buildAdminStyles(false);
+		
+		if( versionNum ){
+			buildVersionStyles(false, versionNum);
+		}else{
+			buildVersionStyles(false, '4');
+			buildVersionStyles(false, '5');
+		}
+	}	
+
+	if( noFlags ){
+		buildAdminStyles(true);
+		buildAdminStyles(false);
+
+		if( versionNum ){
+			buildVersionStyles(true, versionNum);
+			buildVersionStyles(false, versionNum);
+		}else{
+			buildVersionStyles(true, '5');
+			buildVersionStyles(false, '5');
+
+			buildVersionStyles(true, '4');
+			buildVersionStyles(false, '4');
+		}
+	}
+
+}));
+
+// Gulp Task Functions
+async function buildAdminStyles( min = false){
+	var buildOutputStyle = min ? 'compressed' : 'expanded';
+	var minified = min ? '.min' : '';
+
+	var f = [
+		config.themeAssetDir + 'admin.css', 
+		config.templateAssetDir + 'cagov.font-only.css'
+	];
+
+	return gulp.src(f)
 		.pipe(
 			sass({
-				outputStyle: config.outputDevStyle,
+				outputStyle: buildOutputStyle,
 			})
 		)
 		.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('caweb-oceanside.css')) // compiled file
-        .pipe(gulp.dest('./css/version5/'))
-});
-//.pipe(minifycss()) // minify
-        
-/**
- * Task: `styles`.
- *
- * Compiles Sass, Autoprefixes it and Minifies CSS.
- *
- * This task does the following:
- *    1. Gets the source scss file
- *    2. Compiles Sass to CSS
- *    3. Writes Sourcemaps for it
- *    4. Autoprefixes it and generates style.css
- *    5. Renames the CSS file with suffix .min.css
- *    6. Minifies the CSS file and generates style.min.css
- *    7. Injects CSS or reloads the browser via browserSync
- */
-gulp.task( 'styles', () => {
-	return gulp
-		.src( 'assets/css/cagov/version5/cagov.core.css', { allowEmpty: true })
-		.pipe(
-			sass({
-				errLogToConsole: config.errLogToConsole,
-				outputStyle: config.outputStyle,
-				precision: config.precision
-			})
-		)
-		.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
-        .pipe(concat('assets/css/cagov/version5/colorscheme/orange county.css')) // compiled file
-		.pipe( gulp.dest( config.styleDestination ) )
-		.pipe( notify({ message: '\n\n✅  ===> STYLES — completed!\n', onLast: true }) );
-});
+		.pipe(concat('admin' + minified + ' .css')) // compiled file
+		.pipe(gulp.dest('./css/'));
+}
 
+async function buildVersionStyles( min = false, ver = config.templateVer){
+	var buildOutputStyle = min ? 'compressed' : 'expanded';
+	var minified = min ? '.min' : '';
+	var versionDir = config.templateAssetDir + 'version' + ver;
+	var versionColorschemesDir = versionDir + '/colorscheme/';
+	var colors = fs.readdirSync(versionColorschemesDir);
+
+	colors.forEach(function(e){
+		var f = [versionDir + '/cagov.core.css', versionColorschemesDir + e];
+		f = f.concat( config.commonFiles );
+		f = f.concat( versionDir + '/custom.css' );
+
+		var fileName = 'caweb-' + 
+			( minified ? e.replace('.css', '.min.css') : e);
+
+		return gulp.src(f)
+			.pipe(
+				sass({
+					outputStyle: buildOutputStyle,
+				})
+			)
+
+			.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
+			.pipe(concat(fileName )) // compiled file
+			.pipe(gulp.dest('./css/version' + ver + '/'));
+	});
+}
 
 //
 // DEV (Development Output)
 //
-//gulp.task('dev', ['v5oceanside']);
+gulp.task('dev', parameterized.series('caweb-v5-styles --dev', 'caweb-v4-styles --dev', 'caweb-admin-styles --dev') );
 
 // PROD (Minified Output)
-//gulp.task('prod', []);
+gulp.task('prod', parameterized.series('caweb-v5-styles --prod', 'caweb-v4-styles --prod', 'caweb-admin-styles --prod') );
--- wpgulp.config.js
+++ wpgulp.config.js
@@ -9,9 +9,19 @@
 
 
 module.exports = {
+	// CA State Template Options
+	templateVer: '5', // Default CA State Template Version
+
+	// Asset Directories
+	themeAssetDir: '.assets/css/caweb/', // CAWeb CSS 
+	templateAssetDir: '.assets/css/cagov/', // State Template CSS 
+	commonFiles: [ 
+		'.assets/css/cagov/cagov.font-only.css', 
+		'.assets/css/caweb/custom.css'
+	],
 
 	// Project options.
-	projectURL: 'https://caweb.cdt.ca.gov/', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:3000 depending upon your local WordPress setup.
+	/*projectURL: 'https://caweb.cdt.ca.gov/', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:3000 depending upon your local WordPress setup.
 	productURL: './', // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 	browserAutoOpen: false,
 	injectChanges: true,
@@ -23,5 +33,5 @@ module.exports = {
 	outputProdStyle: 'compressed', // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
 	errLogToConsole: true,
 	precision: 10,
-
+	*/
 };
