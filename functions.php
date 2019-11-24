898080a986738c4a4ef5fda67e93fbeee81595d4
Danny Guzman <guzmanjd86@gmail.com>
2019-11-23 21:30:28 -0800

    Updated CAWebVersion to CAWEB_VERSION

--- functions.php
+++ functions.php
@@ -503,11 +503,11 @@ function caweb_admin_enqueue_scripts( $hook ) {
 		wp_enqueue_script('caweb-admin-scripts');
 		
 		// Enqueue Styles
-		wp_enqueue_style('caweb-boot4-toggle', 'https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css', array(), CAWebVersion);
-		wp_enqueue_style('caweb-admin-styles', $adminCSS, array(), CAWebVersion);
-		//wp_enqueue_style('caweb-admin-styles', CAWEB_URI . '/css/admin_custom.css', array(), CAWebVersion);
+		wp_enqueue_style('caweb-boot4-toggle', 'https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css', array(), CAWEB_VERSION);
+		wp_enqueue_style('caweb-admin-styles', $adminCSS, array(), CAWEB_VERSION);
+		//wp_enqueue_style('caweb-admin-styles', CAWEB_URI . '/css/admin_custom.css', array(), CAWEB_VERSION);
 	} elseif (in_array($hook, array('post.php', 'post-new.php', 'widgets.php'))) {
-		wp_enqueue_style('caweb-admin-styles', $adminCSS, array(), CAWebVersion);
+		wp_enqueue_style('caweb-admin-styles', $adminCSS, array(), CAWEB_VERSION);
 	}
 
 	/* Load editor styling */
