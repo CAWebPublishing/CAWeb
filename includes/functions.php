c4adada0037c3f0fac94ce54db611abcefb9dd47
Danny Guzman <guzmanjd86@gmail.com>
2019-11-23 21:28:42 -0800

    Updated CAWebAbsPath to CAWEB_ABSPATH

--- includes/functions.php
+++ includes/functions.php
@@ -655,7 +655,7 @@ function caweb_get_attachment_post_meta( $image_url, $meta_key = '' ) {
 
 function getMinFile( $f, $ext = 'css' ) {
 	// if a minified version exists
-	if( false && file_exists(CAWebAbsPath .  str_replace( ".$ext", ".min.$ext", $f) ) ){
+	if( false && file_exists(CAWEB_ABSPATH .  str_replace( ".$ext", ".min.$ext", $f) ) ){
 		return CAWEB_URI . str_replace( ".$ext", ".min.$ext", $f);
 	}else{
 		return CAWEB_URI . $f;
