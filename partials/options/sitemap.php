6ccb9425f0871f65ca46b3042830ae58850da597
Danny Guzman <guzmanjd86@gmail.com>
2019-12-26 13:58:21 -0800

    Sitemap location displayed if file exist

--- partials/options/sitemap.php
+++ partials/options/sitemap.php
@@ -1,29 +1,14 @@
 <?php
-// set output directory
-
 $directory = wp_upload_dir();
 $file = $directory['basedir'] . '/pdf-word-sitemap.xml';
-
-$site_id = get_current_blog_id();
-
-
-// get correct posts table
-
-if ($site_id === 1) {
-	$wp_posts_table = 'wp_posts';
-} else {
-	$wp_posts_table = 'wp_' . $site_id . '_posts';
-}
-
-// start admin output
-
+$file_url = file_exists($file) ? sprintf('File location: <a href="%1$s%2$s" target="_blank">SiteMap</a>', $directory['baseurl'], '/pdf-word-sitemap.xml') : '';
 ?>
 <div class="p-2 collapse" id="document-sitemap" data-parent="#caweb-settings">
 	<div class="form-row">
 		<div class="form-group col-sm-5">
 			<h2 class="d-inline">Document Sitemap</h2>
 			<button class="doc-sitemap btn btn-primary">Generate</button>
-			<small class="doc-sitemap-update text-muted"></small>
+			<small class="doc-sitemap-update text-muted"><?php print $file_url; ?></small>
 		</div>
 	</div>
 </div>
