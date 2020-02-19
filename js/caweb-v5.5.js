31be595c042998eb294fca9d6b85a2e57aa7b5f8
Danny Guzman <guzmanjd86@gmail.com>
2020-02-19 07:01:51 -0800

    Built asset files

--- js/caweb-v5.5.js
+++ js/caweb-v5.5.js
@@ -19671,7 +19671,7 @@ jQuery(document).ready(function() {
 	Button Element Accessibility 
 	*/
 	
-	var button_elements = $('button:not(.first-level-btn)');
+	var button_elements = $('button:not(.first-level-btn)[role="button"]');
 
 	if( button_elements.length ){
 		button_elements.each(function(index, element) {
