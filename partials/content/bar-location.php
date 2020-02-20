bf8fb87516c733f40010ac5f13e3cc001bc136c3
Danny Guzman <guzmanjd86@gmail.com>
2020-02-19 16:05:06 -0800

    Added missing location bar

---
+++ partials/content/bar-location.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * CAWeb Location Bar
+ *
+ * @package CAWeb
+ */
+
+if ( ! get_option( 'ca_geo_locator_enabled', false ) ) {
+	return;
+}
+?>
+<!-- Location Bar -->
+<div id="locationSettings" class="section section-standout collapse collapsed"></div>
