5683870277d2b7ca15e579becf62f261dc857628
Danny Guzman <guzmanjd86@gmail.com>
2020-02-26 14:43:06 -0800

    admin-post.php 100% compliant with WPCS

--- inc/admin-post.php
+++ inc/admin-post.php
@@ -15,13 +15,18 @@ add_action( 'admin_post_nopriv_caweb_attachment_post_meta', 'caweb_retrieve_atta
  * @return void
  */
 function caweb_retrieve_attachment_post_meta() {
+	$crapm    = wp_create_nonce( 'caweb_retrieve_attachment_post_meta' );
+	$verified = isset( $crapm ) && wp_verify_nonce( sanitize_key( $crapm ), 'caweb_retrieve_attachment_post_meta' );
+
 	if ( ! isset( $_POST['imgs'] ) || empty( $_POST['imgs'] ) || ! is_array( $_POST['imgs'] ) ) {
 		exit();
 	}
 
-	$alts = caweb_get_attachment_post_meta( $_POST['imgs'], '_wp_attachment_image_alt' );
+	$imgs = array_map( 'esc_url_raw', wp_unslash( $_POST['imgs'] ) );
 
-	print json_encode( $alts );
+	$alts = caweb_get_attachment_post_meta( $imgs, '_wp_attachment_image_alt' );
+
+	print wp_json_encode( $alts );
 	exit();
 }
 
