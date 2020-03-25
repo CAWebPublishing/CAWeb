0d5243184c5c7f1659d75d41016e86533f032a85
Danny Guzman <guzmanjd86@gmail.com>
2020-03-25 09:43:58 -0700

    Added sanity check for GA

--- js/frontend.js
+++ js/frontend.js
@@ -1,9 +1,12 @@
 // Google Analytics
-   var _gaq = _gaq || [];
-_gaq.push(['_setAccount', args.ca_google_analytic_id]); // Step 4: your google analytics profile code, either from your own google account, or contact eServices to have one set up for you
-_gaq.push(['_gat._anonymizeIp']);
-_gaq.push(['_setDomainName', '.ca.gov']);
-_gaq.push(['_trackPageview']);
+var _gaq = _gaq || [];
+   
+if("" !== args.ca_google_analytic_id){
+	_gaq.push(['_setAccount', args.ca_google_analytic_id]); // Step 4: your google analytics profile code, either from your own google account, or contact eServices to have one set up for you
+	_gaq.push(['_gat._anonymizeIp']);
+	_gaq.push(['_setDomainName', '.ca.gov']);
+	_gaq.push(['_trackPageview']);
+}
 
 _gaq.push(['b._setAccount', 'UA-3419582-2']); // statewide analytics - do not remove or change
 _gaq.push(['b._setDomainName', '.ca.gov']);
