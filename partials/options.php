bff9476f4ba13e7ea69399d4cf46a6be44808456
Danny Guzman <guzmanjd86@gmail.com>
2020-07-03 15:11:04 -0700

    Corrected sitemap sanity check

--- partials/options.php
+++ partials/options.php
@@ -884,7 +884,7 @@ function caweb_display_sitemap_settings( $is_active = false ) {
 	$file_url  = file_exists( $file ) ? sprintf( 'File location: <a href="%1$s%2$s" target="_blank">Document Map</a>', $directory['baseurl'], '/pdf-word-sitemap.xml' ) : '';
 
 	?>
-	<div class="p-2 collapse<?php print $active ? ' show' : ''; ?>" id="document-sitemap" data-parent="#caweb-settings">
+	<div class="p-2 collapse<?php print $is_active ? ' show' : ''; ?>" id="document-sitemap" data-parent="#caweb-settings">
 		<div class="form-row">
 			<div class="form-group col-sm-12">
 				<h2 class="d-inline">Document Map</h2>
