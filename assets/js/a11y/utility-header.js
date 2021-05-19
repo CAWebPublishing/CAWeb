ccd63029b84796fe3759311983b523fa4b1d4197
Danny Guzman <guzmanjd86@gmail.com>
2021-05-18 17:55:26 -0700

    Removed theme files

--- assets/js/a11y/utility-header.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-	/* -----------------------------------------
-	Utility Header
-	----------------------------------------- */
-	// removing role attribute to fix accessibilty error
-	$(".settings-links button[data-target='#locationSettings']").removeAttr("role");
-});
--- assets/js/wp/theme-customizer/bindings/alert-banners.js
+++
@@ -1,7 +0,0 @@
-jQuery( document ).ready( function($) {
-	wp.customize( 'caweb_alert_banner_0', function( value ) {
-		value.bind( function( newval ) {
-			console.log( $(this).attr('name') );
-		});
-	});
-});
