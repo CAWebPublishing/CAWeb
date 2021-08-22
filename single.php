9fd63b11944570a4373507e0d22b7ee37d92627a
Danny Guzman <guzmanjd86@gmail.com>
2021-08-22 00:51:33 -0700

    Fixed issue with footer bleeding when using The Events Calendar Plugin Fixed issue with Header not loading when using The Events Calendar Plugin Fixed issue with footer bleeding into content on Vanilla WP posts

--- single.php
+++ single.php
@@ -8,7 +8,14 @@
  */
 
 get_header();
-$caweb_is_page_builder_used = function_exists( 'et_pb_is_pagebuilder_used' ) && et_pb_is_pagebuilder_used( get_the_ID() );
+$caweb_is_page_builder_used = caweb_is_divi_used();
+
+// Post Classes.
+$caweb_padding = get_option( 'ca_default_post_date_display' ) ? ' pb-0' : '';
+
+$caweb_post_title_class        = apply_filters( 'caweb_post_title_class', "page-title$caweb_padding" );
+$caweb_post_container_class    = apply_filters( 'caweb_post_container_class', 'page-container' );
+$caweb_post_main_content_class = apply_filters( 'caweb_post_main_content_class', 'main-content' );
 
 ?>
 <body <?php body_class( 'primary' ); ?>>
