c63c5d9f05d2e5bbebca2f2ca2f721c6dd8d87f6
Danny Guzman <guzmanjd86@gmail.com>
2021-12-08 00:26:28 -0800

    Fixed dropdown navigation when using new design system

--- page.php
+++ page.php
@@ -10,10 +10,18 @@
 get_header();
 $caweb_is_page_builder_used = caweb_is_divi_used();
 
+/* CAGov Design System */
+$caweb_enable_design_system = get_option( 'caweb_enable_design_system', false );
+
 // Page Classes.
 $caweb_page_title_class        = apply_filters( 'caweb_page_title_class', 'page-title' );
 $caweb_page_container_class    = apply_filters( 'caweb_page_container_class', 'page-container' );
 $caweb_page_main_content_class = apply_filters( 'caweb_page_main_content_class', 'main-content' );
+
+if( $caweb_enable_design_system ){
+	$caweb_page_container_class .= ' page-container-ds';
+	$caweb_page_main_content_class .= '  main-content-ds';
+}
 
 ?>
 <body <?php body_class( 'primary' ); ?>>
