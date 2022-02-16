070ec827de711dac6180fd78d889edefa5ae2323
kaseycusick <kasey.cusick@state.ca.gov>
2022-02-16 11:17:11 -0800

    fixed wordpress standard errors

--- functions.php
+++ functions.php
@@ -628,7 +628,7 @@ function caweb_save_post_list_meta( $post_id, $post ) {
 function redirect_to_home_if_author_parameter() {
 
 	$is_author_set = get_query_var( 'author', '' );
-	if ( $is_author_set != '' && !is_admin()) {
+	if ( $is_author_set != '' && ! is_admin() ) {
 		wp_redirect( home_url(), 301 );
 		exit;
 	}
