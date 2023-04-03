008ea4f5ddb194a16d8ff831c70e95b0d72796b6
Danny Guzman <guzmanjd86@gmail.com>
2023-04-03 09:57:35 -0700

    Fixed issue with dropdown menu not dropping

--- partials/content/mobile-controls.php
+++ partials/content/mobile-controls.php
@@ -8,6 +8,8 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$caweb_deprecating = '5.5' === $caweb_version;
 ?>
 <!-- mobile navigation controls -->
 <div class="mobile-controls">
@@ -18,7 +20,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<button class="mobile-control toggle-search">
 			<span class="ca-gov-icon-search hidden-print" aria-hidden="true"></span><span class="sr-only">Search</span>
 		</button>
+		<?php if ( $caweb_deprecating ) : ?>
 		<button id="nav-icon3" class="mobile-control toggle-menu" aria-expanded="false" aria-controls="navigation" data-toggle="collapse" data-target="#navigation" data-toggle="collapse" data-target="#navigation">
+		<?php else : ?>	
+		<button id="nav-icon3" class="mobile-control toggle-menu" aria-expanded="false" aria-controls="navigation" data-bs-toggle="collapse" data-bs-target="#navigation" data-bs-toggle="collapse" data-bs-target="#navigation">
+		<?php endif; ?>	
 			<span></span>
 			<span></span>
 			<span></span>
